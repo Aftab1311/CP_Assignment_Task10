6e0dc2d27ab16d3a3ecbeb4c92cba5a6f4fdf44d
Aftab Ahmed <88955526+Aftab1311@users.noreply.github.com>
2021-08-25 19:50:46 +0530

    Update task10.cpp

--- task10.cpp
+++ task10.cpp
@@ -1,3 +1,4 @@
+
 /* Q.2 Given an array A[] consisting of N integers from a range [1, N], the task is to calculate the count of  array elements (non-distinct) that can be represented as the sum of two or more consecutive array  elements. 
  Input: a[] = {3, 1, 4, 1, 5, 9, 2, 6, 5} 
  Output: 5 
